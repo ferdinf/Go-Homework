607ec0f6925e7bfe205b7d464a0933a5530f32af
ferdinf <ferdinfaizi31@gmail.com>
2022-06-19 19:59:49 +0700

    creating config.go file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,20 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+var config Configuration
+
+func LoadConfiguration(filename string) (Configuration, error) {
+	configFile, err := os.Open(filename)
+	defer configFile.Close()
+	if err != nil {
+		return config, err
+	}
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&config)
+	return config, err
+
+}
